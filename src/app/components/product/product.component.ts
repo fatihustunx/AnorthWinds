1a4ed5315db5ac0b1d44fb799058689ae87bf80e
Wistaster <wistaster@hotmail.com>
2023-10-16 02:40:05 +0300

    Added AppModules, HttpClient,  Observers

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
 
-  products : Product [] = [];
+  constructor(private productService:ProductService){}
+
+  products: Product[] = [];
+
+  dataLoaded=false;
+
+  // productResponseModel: ProductResponseModel = {
+  //   data: this.products,
+  //   message: '',
+  //   success: true,
+  // };
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(){
+    this.productService.getProducts().subscribe(response=>{
+      this.products = response.data;
+      this.dataLoaded = true;
+    })
+  }
+ 
 }
