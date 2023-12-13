d73144a5236f451c8137ffd8a8a584fb89f09bed
Wistaster <wistaster@hotmail.com>
2023-12-13 23:17:15 +0300

    Added Reactive Forms.

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ListResponseModel } from '../models/listResponseModel';
 import { Product } from '../models/product';
+import { ResponseModel } from '../models/responseModel';
 
 @Injectable({
   providedIn: 'root'
@@ -22,6 +23,11 @@ export class ProductService {
   getProductsByCategory(categoryId:number) : Observable<ListResponseModel<Product>> {
     let newPath = this.apiUrl + '/products/getbycategory?categoryId=' + categoryId; 
     return this.httpClient.get<ListResponseModel<Product>>(newPath);
+  }
+
+  add(product:Product) : Observable<ResponseModel> {
+    let newPath = this.apiUrl + '/products/add';
+    return this.httpClient.post<ResponseModel>(newPath,product);
   }
   
 }
